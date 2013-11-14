109b4e6398011ac7f0ea91ff5350e07f3859c35b
Clare Curtis <cfcurti2@illinois.edu>
2013-11-13 22:59:03 -0600

    added a bunch of stuff and got the twitter strings working

--- routes.py
+++ routes.py
@@ -1,10 +1,9 @@
 from run import app
 from flask import render_template
-from TwitterAPI import TwitterAPI
 import urllib2
 import urllib
 import json
-import random
+from happy_sad import get_happy_sad
 
 
 @app.route('/')
@@ -18,36 +17,6 @@ def index(location=None):
 		get_location_data(location)
 	curr_string, delta_string = get_happy_sad(location)
 	return render_template("index.html", curr=curr_string, delta=delta_string)
-
-def get_happy_sad(location):
-
-	happy_count, sad_count = get_happy_sad_count(location)
-	#get count
-	#get total
-	#parse strings
-	return happy_count, sad_count
-
-def get_happy_sad_count(location):
-	"""
-	Gets the happy and sad count on the current query
-	Returns the happy count and the sad count
-	"""
-	api = TwitterAPI('SSuspEaugDP7vcITlMA', '6FdUGOTcqlcoufjz62frkKHJ5cgm1vnaIHqjlboOHg', '78925893-xs57TGEPdYpSmbfBKi1XPKVliu5i2TrDRFOEPwVmt', 'fAVXZr4cnEi2hNtVvLyh68mxh4JysMbyJVlBbefhLvf5v')
-	happy_count = 0
-	sad_count = 0
-	happy_query = {'q':':)', 'lang':'en', 'count':'100', 'result_type':'recent'}
-	sad_query = {'q':':(', 'lang':'en', 'count':'100', 'result_type':'recent'}
-	if location:
-		happy_query['locations'] = '{0}, {1}'.format(location[0], location[1])
-		sad_query['locations'] = '{0}, {1}'.format(location[0], location[1])
-	happy = api.request('search/tweets', happy_query)
-	sad = api.request('search/tweets', sad_query)
-	for item in happy:
-		happy_count += 1
-	for item in sad:
-		sad_count += 1
-	return happy_count, sad_count
-
 
 def get_location_data(location):
 	"""
